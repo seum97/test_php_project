11196935343b2924c52340500c325c4f1e99db14
seum97 <seum.cse@aust.edu>
2024-11-21 22:56:17 +0600

    delete.php updated

--- delete.php
+++ delete.php
@@ -12,8 +12,12 @@ $sql = "DELETE FROM `users` WHERE id = $id";
 $result = mysqli_query($conn, $sql);
 
 if ($result) {
-  header("Location: index.php?msg=Data deleted successfully");
-  // header("Location: index.php");
+  // header("Location: index.php?msg=Data deleted successfully");
+
+    session_start();
+    $_SESSION['msg'] = "User deleted successfully";
+    header("Location: index.php");
+    exit();
 } else {
   echo "Failed: " . mysqli_error($conn);
 }
--- index.php
+++ index.php
@@ -14,15 +14,16 @@ include "db_connection.php";
 	<div class="container">
 
 		<h1>User Management System</h1> 
-		<?php
-		    if (isset($_GET["msg"])) {
-		      $msg = $_GET["msg"];
-		      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">'
-		       .$msg.
+		    <?php
+			session_start();
+			if (isset($_SESSION['msg'])) {
+			    echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">'
+		       .$_SESSION['msg'].
 		      '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		    </div>';
-		    }
-		    ?>
+			    unset($_SESSION['msg']); // Clear the message
+			}
+			?>
 		<a href="create_user.php" class="btn btn-dark mb-3">Create New User</a>
 
 		<table class="table table-striped">
